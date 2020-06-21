8fb0b8aa4f90c9cc512e6c521f262a2403cb9110
LailyNurH <lailyhamidah3005@gmail.com>
2020-06-21 21:46:23 +0700

    PUSH FILE , 21/06/2020

--- app/src/main/java/com/example/afinal/komunikasi.java
+++ app/src/main/java/com/example/afinal/komunikasi.java
@@ -1,5 +1,6 @@
 package com.example.afinal;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class komunikasi extends Fragment {
+    ImageButton btn_pik;
 
     public komunikasi() {
         // Required empty public constructor
@@ -23,6 +26,7 @@ public class komunikasi extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_komunikasi, container, false);
     }
 }
